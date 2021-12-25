5da365412601a19b3d504dc5e12f982f7971731c
liou <liou666@126.con>
2021-12-25 18:22:24 +0800

    fix: Adjust code structure

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,45 +3,30 @@
  * @Autor: Liou
  * @Date: 2021-12-19 15:46:59
  * @LastEditors: Liou
- * @LastEditTime: 2021-12-25 17:48:22
+ * @LastEditTime: 2021-12-25 18:21:32
  */
 import { memo, useState } from "react";
 import { Link, NavLink } from "react-router-dom";
 import { Input } from "antd"
 import { SearchOutlined } from '@ant-design/icons'
-
+import { headerLinks } from "@/common/localData"
 import {
     HeaderWrapper,
     HeaderLeft,
     HeaderRight
 } from './style';
-{/* <Link to="/" >发现音乐</Link>
-            <Link to="/my" >我的音乐</Link>
-            <Link to="/discover" >推荐</Link>
-            <Link to="/discover/toplist" >排行榜</Link>
-            <Link to="/discover/playlist" >歌单</Link>
-            <Link to="/discover/djradio" >主播电台</Link>
-            <Link to="/discover/artist" >歌手</Link>
-            <Link to="/discover/album" >新碟上架</Link> */}
-const navs = [
-    { title: "发现音乐", path: "/" },
-    { title: "我的音乐", path: "/my" },
-    { title: "关注", path: "/friend" },
-    { title: "商城", path: "https://music.163.com/store/product" },
-    { title: "音乐人", path: "https://music.163.com/st/musician" },
-    { title: "下载客户端", path: "/download" },
-]
+
 
 const showSelectItem = (item, index) => {
     if (index < 3) {
         return (
-            <NavLink activeClassName="active" to={item.path}>
+            <NavLink to={item.link}>
                 {item.title}
                 <i className="sprite_01 icon"></i>
             </NavLink>
         )
     } else {
-        return <a href={item.path}>{item.title}</a>
+        return <a href={item.link}>{item.title}</a>
     }
 }
 export default memo(() => {
@@ -52,7 +37,7 @@ export default memo(() => {
                     <a href="#/" className="logo sprite_01">网易云音乐</a>
                     <div className="select-list">
                         {
-                            navs.map((item, index) => {
+                            headerLinks.map((item, index) => {
                                 return (
                                     <div key={item.title} className={"select-item"}>
                                         {showSelectItem(item, index)}
--- src/pages/FindMusic/Recommend/index.js
+++ src/pages/FindMusic/Recommend/index.js
@@ -3,16 +3,30 @@
  * @Autor: Liou
  * @Date: 2021-12-19 15:50:51
  * @LastEditors: Liou
- * @LastEditTime: 2021-12-24 20:33:03
+ * @LastEditTime: 2021-12-25 15:47:14
  */
 
-import { memo } from "react";
-import { Link } from "react-router-dom"
+import { memo, useEffect } from "react";
+
+import { useSelector, useDispatch, shallowEqual } from "react-redux"
+
+import { getTopBannerAction } from "./store/actionCreator"
+
+
 export default memo(() => {
+    const { topBanners } = useSelector(state => ({
+        topBanners: state.recommend.topBanners
+    }), shallowEqual);
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        dispatch(getTopBannerAction())
+    }, [dispatch])
+
     return (
         <>
             <h1>home recommend page</h1>
-
+            <span>{topBanners.length}</span>
         </>
     )
 })
