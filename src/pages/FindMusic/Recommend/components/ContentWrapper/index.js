f91e10cd6829c56e13ee7a176524069f0e14f3d3
liou <liou666@126.con>
2021-12-26 18:01:31 +0800

    fix: 样式调整

--- src/pages/FindMusic/Recommend/components/ContentWrapper/index.js
+++ src/pages/FindMusic/Recommend/components/ContentWrapper/index.js
@@ -3,16 +3,16 @@
  * @Autor: Liou
  * @Date: 2021-12-26 14:28:01
  * @LastEditors: Liou
- * @LastEditTime: 2021-12-26 14:41:56
+ * @LastEditTime: 2021-12-26 17:59:53
  */
-import { memo} from "react";
+import { memo } from "react";
 
 export default memo((props) => {
     const [LeftContent, RightContent] = props.children
     return (
         <main className="content-wrap">
             <div style={{ display: 'flex', width: "100%" }}>
-                <div style={{ padding: "20px 20px 40px", width: "730px" }}>
+                <div style={{ padding: "20px 20px 40px", border: "2px solid #e4e4e4", width: "730px" }}>
                     {LeftContent}
                 </div>
                 <div style={{ padding: "20px 20px 40px", flex: "1" }}>
