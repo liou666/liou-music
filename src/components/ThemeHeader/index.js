cb1ccde5fb1d681326f21b2ce2ef038887103b60
liou <liou666@126.con>
2021-12-26 16:45:14 +0800

    feat: 增加props默认值

--- src/components/ThemeHeader/index.js
+++ src/components/ThemeHeader/index.js
@@ -3,17 +3,18 @@
  * @Autor: Liou
  * @Date: 2021-12-26 15:02:31
  * @LastEditors: Liou
- * @LastEditTime: 2021-12-26 15:33:50
+ * @LastEditTime: 2021-12-26 16:44:08
  */
 import react, { memo } from "react";
 import { ThemeHeaderWrap } from "./style"
-
-export default memo(({ title, keywords }) => {
+import PropTypes from "prop-types"
+const ThemeHeader = memo(({ title, keywords }) => {
     return (
         <ThemeHeaderWrap>
             <div className="header-title">
                 <h3>{title}</h3>
             </div>
+
             <div className="other-info">
                 {keywords.map((item, index) => {
                     return (
@@ -21,8 +22,18 @@ export default memo(({ title, keywords }) => {
                     )
                 })}
             </div>
+
             <a className="more-btn">更多</a>
             <i className="icon sprite_02"></i>
         </ThemeHeaderWrap>
     )
 })
+
+ThemeHeader.propTypes = {
+    title: PropTypes.string.isRequired,
+    keywords: PropTypes.array
+}
+ThemeHeader.defaultProps = {
+    keywords: []
+}
+export default ThemeHeader
